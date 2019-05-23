7156c1c075ee2ebb3f11d1435f07a278660f7bc4
CRS19 <39920116+CRS19@users.noreply.github.com>
2019-05-23 10:41:22 -0500

    Cambio del access token

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
         userId: '1585344637', //Es el id del usurio del cual se quiere que se muestren las fotos
         limit: 12,
         resolution: 'standard_resolution',
-        accessToken: '1585344637.1677ed0.3b30b815079642a58665bb34a9013356', // creado en intagram.pixelunion.net
+        accessToken: '1677ed0.3b30b815079642a58665bb34a9013356', // creado en intagram.pixelunion.net
         sortBy: 'most-recent',
         template: '<div class="col-lg-3 instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
     });
