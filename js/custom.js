19296a87b9d1662f2c784de553d8acf33b1f554f
CRS19 <39920116+CRS19@users.noreply.github.com>
2019-05-27 17:33:25 -0500

    cambio parece que ya

--- js/custom.js
+++ js/custom.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
 
     var userFeed = new Instafeed({
         get: 'user',
-        userId: '8987997106',
-        clientId: '924f677fa3854436947ab4372ffa688d',
-        accessToken: '8987997106.924f677.8555ecbd52584f41b9b22ec1a16dafb9',
+        userId: '1585344637',
+        clientId: '2d758da2492d42eea132bf2d0f9000d3',
+        accessToken: '1585344637.2d758da.d9754909aa17467d829aebaa761097ff',
         resolution: 'standard_resolution',
         template: '<a href="{{link}}" target="_blank" id="{{id}}"><img src="{{image}}" /></a>',
         sortBy: 'most-recent',
